db4a534493c0ba54e83444616a353fc8ee280a05
ChurchillV <vinchurch70@gmail.com>
2023-06-08 15:38:17 +0000

    Constructor function in Lab4

--- labwork/lab4.cpp
+++ labwork/lab4.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class Quadrilateral {
     private: //These are members that are only accessible within the scope of the class
         int x,y;
-        
+
     public: //These can be accessed from anywhere within the programme
-        void set_values(int a, int b) { //Set the values of x and y to a and b respectively
+        Quadrilateral(int a, int b) { //Set the values of x and y to a and b respectively
             x = a;
             y = b;
         }
@@ -18,9 +18,9 @@ class Quadrilateral {
 
 int main() {
     //Create two objects from the Quadrilateral class
-    Quadrilateral square, rectangle;
-    square.set_values(10,10);
-    rectangle.set_values(5,10);
+    Quadrilateral square(10,10), rectangle(5,10);
+    // square.set_values(10,10);
+    // rectangle.set_values(5,10);
 
     cout << "Area of rectangle: " << square.area() << endl;
     cout << "Area of square: " << rectangle.area() << endl;
