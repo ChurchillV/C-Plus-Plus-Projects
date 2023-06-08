87459c77a8215ae1d6cfbae59eb99fe35e990ca9
ChurchillV <vinchurch70@gmail.com>
2023-06-08 15:18:44 +0000

    Lab 4

---
+++ labwork/lab4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Create the class Quadrilateral
+class Quadrilateral {
+    private: //These are members that are only accessible within the scope of the class
+        int x,y;
+    public: //These can be accessed from anywhere within the programme
+        void set_values(int a, int b) { //Set the values of x and y to a and b respectively
+            x = a;
+            y = b;
+        }
+        float area() { //Calculate the area of the given quadrilateral
+            return x*y;
+        }
+};
+
+int main() {
+    Quadrilateral square, rectangle;
+    square.set_values(10,10);
+    rectangle.set_values(5,10);
+
+    cout << "Area of rectangle: " << square.area() << endl;
+    cout << "Area of square: " << rectangle.area() << endl;
+    return 0;
+}
