1ce00afccb4ce841ba28c936152fc8fcc129ebdd
ChurchillV <vinchurch70@gmail.com>
2023-06-12 18:02:23 +0000

    Changes

--- Cpp-practice/word_conversion.cpp
+++ Cpp-practice/word_conversion.cpp
@@ -13,26 +13,19 @@ int main() {
     conversion(input2);
     return 0;
 }
-
-void conversion(int input) {
-    if(input < 1 || input > 9) {
-        classify(input);
-}
-    else if (input == 1) cout << endl << "one";
-    else if (input == 2) cout << endl << "two";
-    else if (input == 3) cout << endl << "three";
-    else if (input == 4) cout << endl << "four";
-    else if (input == 5) cout << endl << "five";
-    else if (input == 6) cout << endl << "six";
-    else if (input == 7) cout << endl << "seven";
-    else if (input == 8) cout << endl << "eight";
-    else if (input == 9) cout << endl << "nine";
-    else cout << endl << "Invalid input";
-}
-
+//Classify inputs as even or odd
 void classify(int input) {
     if(input %2 == 0) {
             cout << endl << "Even";
         }
         else cout << endl << "Odd";
+}
+
+//Convert inputs to strings
+void conversion(int input) {
+    string values[9] = {"one","two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    if(input < 1 || input > 9) {
+        classify(input);
+}
+    else cout <<endl << values[input-1];
 }
