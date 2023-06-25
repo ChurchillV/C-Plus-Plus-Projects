f7c50572983d9461222bb7b95f0be0bac201f790
ChurchillV <vinchurch70@gmail.com>
2023-06-25 21:09:14 +0000

    TollBooth exercise

---
+++ Cpp-practice/tollbooth.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+class tollBooth {
+    private:
+        unsigned int no_of_cars;
+        double cash_total;
+
+    public:
+        tollBooth() {
+            no_of_cars = 0;
+            cash_total = 0;
+        };
+        void payingCar() {
+            no_of_cars += 1;
+            cash_total += 0.50;
+        };
+        void nopayCar() {
+            no_of_cars += 1;
+        };
+        void display() const {
+            cout << "Cars gone past: " << no_of_cars; 
+            cout << '\t' << "Total funds received: GHC " << 
+            cash_total << endl;
+        }
+};
+
+int main() {
+    tollBooth booth1;
+    cout << "TollBooth Management System" << endl
+    << "Press 'y' for paying cars" << endl <<
+    "Press 'n' for non-paying cars" << endl <<
+    "Press 'esc' to end programme and display total number of cars and funds gathered" << endl;
+    const int ESC = 27;
+    const int y = 121;
+    const int n = 110;
+    bool exit_program = false;
+
+    while (!exit_program) {
+    int c = getch();
+    if (c == y) {
+        booth1.payingCar();
+        cout << "Paying Car Added" << endl;
+    }
+    if(c == n) {
+        booth1.nopayCar();
+        cout << "Non-paying Car Added" << endl; 
+    }
+    if(c == ESC) {
+        booth1.display();
+        exit_program = true;
+    }}
+    return 0;
+}
